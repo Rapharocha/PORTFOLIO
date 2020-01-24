fcc002b899fe9348316c591cfc7a13da9b8131fa
Rapharocha <rapharocha2015@hotmail.com>
2020-01-24 14:54:44 -0300

    portfolio base

--- src/main/java/com/gft/portfolio/controller/PortfolioController.java
+++ src/main/java/com/gft/portfolio/controller/PortfolioController.java
@@ -2,16 +2,34 @@ package com.gft.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-
+@RequestMapping("/portfolio")
 public class PortfolioController {
-
+	
+  	
 	@RequestMapping("/home")
-	public String novo() {
-		
-		return "CadastroPortfolio";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroPortfolio");
+		return mv;
 	}
 	
+	@RequestMapping("/projetos")
+	public ModelAndView projetos() {
+		ModelAndView mv = new ModelAndView("ProjetosPortfolio");
+		return mv;
+	}
 	
+	@RequestMapping("/skills")
+	public ModelAndView skill() {
+		ModelAndView mv = new ModelAndView("Skills");
+		return mv;
+	}
+	
+	@RequestMapping("/contato")
+	public ModelAndView contato() {
+		ModelAndView mv = new ModelAndView("Contato");
+		return mv; 
+	}
 }
